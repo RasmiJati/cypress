12a8f424d4329afd17e3005f866a966b48a401eb
RasmiJati <jatirasmi65@gmail.com>
2024-09-11 07:39:24 +0545

    written first command , cypress studio , selectors , interaction

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://rasmijati.com.np',
+    viewportHeight: 550,
+    viewportWidth:660,
+    experimentalStudio: true
+
   },
 });
